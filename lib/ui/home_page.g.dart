94eab865837b57799859b82a4ad2d824651e440f
Rémon <remonhelmond@gmail.com>
2020-01-05 14:23:42 +0100

    add cached network image

--- lib/ui/home_page.g.dart
+++ lib/ui/home_page.g.dart
@@ -7,24 +7,24 @@ part of 'home_page.dart';
 // **************************************************************************
 
 abstract class $HomePageViewModel {
-  Iterable<NationalHoliday> get holidays;
+  NationalHoliday get holiday;
   const $HomePageViewModel();
-  HomePageViewModel copyWith({Iterable<NationalHoliday> holidays}) =>
-      HomePageViewModel(holidays: holidays ?? this.holidays);
-  String toString() => "HomePageViewModel(holidays: $holidays)";
+  HomePageViewModel copyWith({NationalHoliday holiday}) =>
+      HomePageViewModel(holiday: holiday ?? this.holiday);
+  String toString() => "HomePageViewModel(holiday: $holiday)";
   bool operator ==(dynamic other) =>
       other.runtimeType == runtimeType &&
-      const DeepCollectionEquality.unordered().equals(holidays, other.holidays);
+      const DeepCollectionEquality.unordered().equals(holiday, other.holiday);
   @override
   int get hashCode {
     var result = 17;
     result =
-        37 * result + const DeepCollectionEquality.unordered().hash(holidays);
+        37 * result + const DeepCollectionEquality.unordered().hash(holiday);
     return result;
   }
 }
 
 class HomePageViewModel$ {
-  static final holidays = Lens<HomePageViewModel, Iterable<NationalHoliday>>(
-      (s_) => s_.holidays, (s_, holidays) => s_.copyWith(holidays: holidays));
+  static final holiday = Lens<HomePageViewModel, NationalHoliday>(
+      (s_) => s_.holiday, (s_, holiday) => s_.copyWith(holiday: holiday));
 }
